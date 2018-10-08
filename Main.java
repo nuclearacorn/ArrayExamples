934e6f38391d1f943b94bda9a552fa76933ba05a
nuclearacorn <31290639+nuclearacorn@users.noreply.github.com>
2018-10-08 11:44:39 -0700

    Change 1
    Did Task 1 and 2. Both were successful.

--- Main.java
+++ Main.java
@@ -30,13 +30,23 @@ class Main
          *          call this array ar1.  Print it out below.
          */
         System.out.println("*** PRINTING ar1 *");
+        int [] ar1;
+        ar1 = new int[n];
+        
+        for (int i=0; i<ar1.length; i++)
+            ar1[i]=i;
+        for (int i=0; i<ar1.length; i++)
+            System.out.println("ar["+i+"] = "+ar1[i] );
         
                 
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
-        
+        int [] ar2;
+        ar2 = ar1;
+        for (int i=0; i<ar1.length; i++)
+            System.out.println("ar["+i+"] = "+ar2[i] );
         
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
